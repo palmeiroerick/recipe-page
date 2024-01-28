18caacdce24212cf6ff0033b9ba4edf114bad1e0
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-27 22:30:16 -0300

    feat: add subtitle component

---
+++ components/SubTitle/index.tsx
@@ -0,0 +1,7 @@
+import GenericChildren from "@/types/GenericChildren";
+
+const SubTitle = ({ children }: GenericChildren) => {
+  return <h2 className="text-3xl font-youngSerif text-nutmeg">{children}</h2>;
+};
+
+export default SubTitle;
