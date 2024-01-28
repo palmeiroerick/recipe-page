29a9aade320d2f61f7b0cc704cd1a5f67ad7d65c
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-27 22:27:59 -0300

    feat: add strong text component

---
+++ components/StrongText/index.tsx
@@ -0,0 +1,7 @@
+import GenericChildren from "@/types/GenericChildren";
+
+const StrongText = ({ children }: GenericChildren) => {
+  return <span className="font-bold text-wengeBrown">{children}</span>;
+};
+
+export default StrongText;
