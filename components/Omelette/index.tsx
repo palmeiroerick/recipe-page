c81ebf550f4c4b3f4a3a5c7fbd829e3a57169341
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-28 16:20:22 -0300

    style: add desktop styles to omelette component

--- components/Omelette/index.tsx
+++ components/Omelette/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Omelette = () => {
   return (
-    <div className="relative h-[10.75rem] w-[23.438rem]">
+    <div className="relative h-[10.75rem] w-[23.438rem] lg:m-auto lg:mt-10 lg:h-[18.25rem] lg:w-[40rem] lg:overflow-hidden lg:rounded-2xl">
       <Image
         className="object-contain"
         src="/assets/image-omelette.jpeg"
